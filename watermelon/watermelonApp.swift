28ca60d8e5f381efa98941c826534eb4b9c9610e
Hyung Lee <hwlee2@gmail.com>
2023-10-25 22:31:44 -0700

    first msg

---
+++ watermelon/watermelonApp.swift
@@ -0,0 +1,17 @@
+//
+//  watermelonApp.swift
+//  watermelon
+//
+//  Created by Hyung Lee on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct watermelonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
